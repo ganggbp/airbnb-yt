7abcbec392702cabd2d5092b78cb30831e685ad2
gangg <burit_bk@hotmail.com>
2023-06-02 14:01:35 +0700

    finish categories UI

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,13 +1,14 @@
 'use client';
 
-import { User } from '@prisma/client';
+import { SafeUser } from '@/app/types';
 import Container from '../Container';
 import Logo from './Logo';
 import Search from './Search';
 import UserMenu from './UserMenu';
+import Categories from './Categories';
 
 interface NavbarProps {
-  currentUser?: User | null;
+  currentUser?: SafeUser | null;
 }
 
 const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
@@ -18,10 +19,11 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
             <Logo />
             <Search />
-            <UserMenu currentUser={currentUser}/>
+            <UserMenu currentUser={currentUser} />
           </div>
         </Container>
       </div>
+      <Categories />
     </div>
   );
 };
